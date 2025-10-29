765696710e803837fe37aedd4e7ee06c66a7c7b9
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-10-29 23:11:45 +0530

    code is add to codebase up to section 02

---
+++ style.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const icon = document.querySelector('#hamburger i');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('mobile-list-action');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-xmark');
+})
