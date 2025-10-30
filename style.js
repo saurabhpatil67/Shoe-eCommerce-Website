9ccaacbeb008fb31539b98ce303bb7753f213d26
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-10-30 18:01:31 +0530

    Add code up to footer and complete project

--- style.js
+++ style.js
@@ -1,10 +1,9 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-const icon = document.querySelector('#hamburger i');
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const icon = document.querySelector("#hamburger i");
 
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('mobile-list-action');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-xmark');
-})
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("mobile-list-action");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+});
